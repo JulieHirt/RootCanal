4ac80ce141cf9ed04932335f237a48fea4c00e1f
JulieBodette <jr3python@gmail.com>
2023-02-04 12:35:11 -0500

    Update Bacteria.cs

--- Assets/RootCanal/Scripts/Bacteria.cs
+++ Assets/RootCanal/Scripts/Bacteria.cs
@@ -8,13 +8,27 @@ namespace RootCanal
     {
         //TODO: give player the ability to upgrade this
         public float Speed = 10f;
+        Vector2 lastClickedPos;
+        bool moving;
 
         // Update is called once per frame
         void Update()
         {
             if(Input.GetMouseButtonDown(0))
             {
-Debug.Log("hey");
+                lastClickedPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                moving = true;
+            }
+            if(moving && (Vector2)transform.position != lastClickedPos)
+            {
+                float step = Speed*Time.deltaTime;
+                transform.position = Vector2.MoveTowards(transform.position, lastClickedPos, step);
+            }
+            else
+            {
+                {
+                    moving = false;
+                }
             }
         
         }
