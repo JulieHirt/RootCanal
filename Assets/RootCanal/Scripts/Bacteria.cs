66cecfd6a87a91893fade1f150082d978c6ab8a4
JulieBodette <jr3python@gmail.com>
2023-02-04 00:41:46 -0500

    enabled both old and new input systems
    simple code that says hey when you click the screen

--- Assets/RootCanal/Scripts/Bacteria.cs
+++ Assets/RootCanal/Scripts/Bacteria.cs
@@ -6,15 +6,16 @@ namespace RootCanal
 {
     public class Bacteria : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
-        {
-        
-        }
+        //TODO: give player the ability to upgrade this
+        public float Speed = 10f;
 
         // Update is called once per frame
         void Update()
         {
+            if(Input.GetMouseButtonDown(0))
+            {
+Debug.Log("hey");
+            }
         
         }
     }
