1f3d42b8670b74bf69760a8f1a4f1b19d3404c80
JulieBodette <jr3python@gmail.com>
2023-02-04 13:46:03 -0500

    bacteria changes color when selected
    need to change this so it has selection circle/arrow/image instead
    color change is reserved for health

--- Assets/RootCanal/Scripts/Bacteria.cs
+++ Assets/RootCanal/Scripts/Bacteria.cs
@@ -6,11 +6,19 @@ namespace RootCanal
 {
     public class Bacteria : MonoBehaviour
     {
+        SpriteRenderer m_SpriteRenderer;
         //TODO: give player the ability to upgrade this
         public float Speed = 10f;
         Vector2 lastClickedPos;
         bool moving;
         bool selected;//detects if the player has selected the bacteria to give commands to it
+
+        void Start()
+    {
+        //Fetch the SpriteRenderer from the GameObject
+        m_SpriteRenderer = GetComponent<SpriteRenderer>();
+
+    }
 
         // Update is called once per frame
         void Update()
@@ -36,6 +44,13 @@ namespace RootCanal
     void OnMouseDown()//detect if the player has clicked on the bacteria to select it
     {//toggle if the bacteria is selected
         selected = !selected;
+        if(selected)
+        {
+            m_SpriteRenderer.color = Color.yellow;
+        }
+        else{
+            m_SpriteRenderer.color = Color.white;
+        }
     }
     }
 }
