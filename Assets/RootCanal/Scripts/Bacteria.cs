d01db1bc537df48517aba81e71814d5a71168bd8
JulieBodette <jr3python@gmail.com>
2023-02-04 19:01:00 -0500

    uses janky float coordinates but at least it compiles

--- Assets/RootCanal/Scripts/Bacteria.cs
+++ Assets/RootCanal/Scripts/Bacteria.cs
@@ -2,17 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.Tilemaps;
 
 namespace RootCanal
 {
     public class Bacteria : MonoBehaviour
     {
+        public Tilemap tm;//set this in the inspector. References the tilemap from the world prefab.
         Transform selectionSprite;
         SpriteRenderer m_SpriteRenderer;
         //TODO: give player the ability to upgrade this
         public float Speed = 10f;
         Vector2 lastClickedPos;
         Vector2 prevPos; //keep track of position in prev frame to determine if moving left or right
+        Vector3Int tilePos;
         bool moving;
         bool selected;//detects if the player has selected the bacteria to give commands to it
         public UnityEvent<Vector3Int> DestinationReached = new UnityEvent<Vector3Int>();
@@ -33,6 +36,7 @@ namespace RootCanal
             if(Input.GetMouseButtonDown(0) && selected == true)
             {
                 lastClickedPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                tilePos = tm.WorldToCell(lastClickedPos);
                 moving = true;
             }
             if(moving && (Vector2)transform.position != lastClickedPos)
@@ -56,7 +60,7 @@ namespace RootCanal
                     moving = false;
                     //raise an event
                     //todo:fix this so the position is where you sent it
-                    DestinationReached.Invoke( new Vector3Int(0,0,0));
+                    DestinationReached.Invoke( tilePos);
                 }
             }
             prevPos = transform.position; //set the prevPos for the next update cycle
