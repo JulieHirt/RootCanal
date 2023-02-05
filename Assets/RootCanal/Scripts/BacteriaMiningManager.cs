dc23347550e77f7205c0c1cec0d92c8d519a35d0
Dan Vicarel <shundra8820@gmail.com>
2023-02-05 13:44:08 -0500

    All tooth "tissues" show correct breaking sprites now

--- Assets/RootCanal/Scripts/BacteriaMiningManager.cs
+++ Assets/RootCanal/Scripts/BacteriaMiningManager.cs
@@ -2,13 +2,25 @@
 
 using Sirenix.OdinInspector;
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
 namespace RootCanal
 {
+    [Serializable]
+    public class ToothTissueConfig
+    {
+        public string Name = "";
+        public int MaxDurability = 100;
+        public int DamagePerHit = 10;
+        public Tile[] DamageTiles = Array.Empty<Tile>();
+    }
+
     public class BacteriaMiningManager : MonoBehaviour
     {
+        private readonly Dictionary<string, ToothTissueConfig> _toothTissueConfigByName = new();
+
         [Required] public Tilemap? Tilemap;
         [Required] public TileInstanceManager? TileInstanceManager;
         [Required] public BacteriaManager? BacteriaManager;
@@ -16,8 +28,7 @@ namespace RootCanal
         [Required] public QuantityContext? MoneyContext;
 
         [Header("Mining effects")]
-        public int DamagePerHit = 10;
-        public Tile[] DamageTiles = Array.Empty<Tile>();
+        public ToothTissueConfig[] ToothTissueConfigs = Array.Empty<ToothTissueConfig>();
 
         [Header("Mining rewards")]
         public int MinMoneyPerTile = 1;
@@ -25,15 +36,26 @@ namespace RootCanal
 
         private void Awake()
         {
+            foreach (ToothTissueConfig toothTissueConfig in ToothTissueConfigs)
+                _toothTissueConfigByName[toothTissueConfig.Name] = toothTissueConfig;
+
             BacteriaManager!.BacteriumAdded.AddListener(bacterium =>
                 bacterium.MiningTimer!.Triggered.AddListener(() => mine(bacterium)));
 
             TileInstanceManager!.TileInstanceCreated += onTileInstanceCreated;
         }
 
-        private void onTileInstanceCreated(object sender, (TileInstance tileInstance, Vector3Int position) e) =>
+        private void onTileInstanceCreated(object sender, (TileInstance tileInstance, Vector3Int position) e)
+        {
+            TileBase tile = Tilemap!.GetTile(e.position);
+            ToothTissueConfig toothTissueConfig = _toothTissueConfigByName[getTissueNameFromTileName(tile.name)];
+            e.tileInstance.Durability!.MaxAmount = toothTissueConfig.MaxDurability;
+
             e.tileInstance.Durability!.AmountChanged.AddListener(delta =>
                 onTileInstanceBroken(e.tileInstance, e.position));
+        }
+
+        private string getTissueNameFromTileName(string tileName) => tileName.Substring(0, tileName.IndexOf("_"));
 
         private void onTileInstanceBroken(TileInstance tileInstance, Vector3Int position)
         {
@@ -53,17 +75,19 @@ namespace RootCanal
                 Debug.LogError($"Bacterium {bacterium.name} has no cell to mine", context: bacterium);
                 return;
             }
-            TileInstance? tile = TileInstanceManager!.GetTileAtPosition(mineCell.Value);
-            if (tile == null) {
+            TileInstance? tileInstance = TileInstanceManager!.GetTileAtPosition(mineCell.Value);
+            if (tileInstance == null) {
                 Debug.LogError($"Bacterium {bacterium.name} is mining a cell with no {nameof(TileInstance)}", context: bacterium);
                 return;
             }
 
-            Debug.Log($"Bacterium {bacterium.name} decreasing durability of tile instance {tile.name} by {DamagePerHit}...");
-            tile.Durability!.AddToAmount(-DamagePerHit);
-            if (tile.Durability.CurrentAmount > tile.Durability.MinAmount) {
-                int damageTileIndex = (int)(tile.Durability!.CurrentAmountFraction * DamageTiles.Length);
-                Tilemap!.SetTile(mineCell.Value, DamageTiles[damageTileIndex]);
+            TileBase tile = Tilemap!.GetTile(mineCell.Value);
+            ToothTissueConfig toothTissueConfig = _toothTissueConfigByName[getTissueNameFromTileName(tile.name)];
+            Debug.Log($"Bacterium {bacterium.name} decreasing durability of tile instance {tileInstance.name} by {toothTissueConfig.DamagePerHit}...");
+            tileInstance.Durability!.AddToAmount(-toothTissueConfig.DamagePerHit);
+            if (tileInstance.Durability.CurrentAmount > tileInstance.Durability.MinAmount) {
+                int damageTileIndex = (int)(tileInstance.Durability!.CurrentAmountFraction * toothTissueConfig.DamageTiles.Length - 1);
+                Tilemap!.SetTile(mineCell.Value, toothTissueConfig.DamageTiles[damageTileIndex]);
             }
         }
     }
