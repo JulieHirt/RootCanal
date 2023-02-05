611545cdaed283340d590ebd59cc93c0958fe367
Alex-BoopyLoops-Christian <starttoast@gmail.com>
2023-02-05 15:02:14 -0500

    MENU NAVIGATION

---
+++ Assets/RootCanal/Scripts/MainMenu.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace RootCanal
+{
+    public class MainMenu : MonoBehaviour
+    {
+        public void Home(string sceneName)
+        {
+            
+            SceneManager.LoadScene(sceneName);
+        }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
